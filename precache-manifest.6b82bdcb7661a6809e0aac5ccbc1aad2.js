8c8c1bdc0d94b47641ef6df0e290e7f48b358788
Ripal Barot <ripplejb@hotmail.com>
2018-11-14 00:20:12 -0600

    Updates

---
+++ precache-manifest.6b82bdcb7661a6809e0aac5ccbc1aad2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Toastmasters-Grammarian-Sheet/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Toastmasters-Grammarian-Sheet/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Toastmasters-Grammarian-Sheet/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Toastmasters-Grammarian-Sheet/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Toastmasters-Grammarian-Sheet/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "61b42bd559212ca4a00c",
+    "url": "/Toastmasters-Grammarian-Sheet/static/js/runtime~main.61b42bd5.js"
+  },
+  {
+    "revision": "21ea7bef312c34741009",
+    "url": "/Toastmasters-Grammarian-Sheet/static/js/main.21ea7bef.chunk.js"
+  },
+  {
+    "revision": "f5ad5488621beabcbf2f",
+    "url": "/Toastmasters-Grammarian-Sheet/static/js/1.f5ad5488.chunk.js"
+  },
+  {
+    "revision": "21ea7bef312c34741009",
+    "url": "/Toastmasters-Grammarian-Sheet/static/css/main.206c9641.chunk.css"
+  },
+  {
+    "revision": "8f2f31f292ba0af58b08c838e6f58ae4",
+    "url": "/Toastmasters-Grammarian-Sheet/index.html"
+  }
+];
